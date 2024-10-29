fb6a3a43a107a391a9ce120655bbcfcdec47f9e5
unknown <ambreenjahan144@gmail.com>
2024-10-29 20:22:18 +0500

    style

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-
-
-import style from "/style/global.module.css";
+//import style from "/style/global.module.css"
 import Hero from "./hero/page";
 import Blogs from "./blog/page";
 import Featured from "./work/page";
 import Footer from "./contact/page";
+import Navbar from "./navbar/page";
 
 
 export default function Home() {
   
   return (
     <div >
-      
+      <Navbar/>
       <Hero/>
       <Blogs/>
       <Featured/>
